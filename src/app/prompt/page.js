f094b488294077547296474bf4b06248464a95a4
rholibobo <rholibobo@gmail.com>
2023-06-09 16:16:19 +0100

    made changes

--- src/app/prompt/page.js
+++ src/app/prompt/page.js
@@ -1,24 +1,13 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-import app from "../../../firebase";
-import getPromptData from "../api/getPrompt"
-
-const firestore = getFirestore(app);
+import getPromptData from "../api/getPrompt";
 
 const Page = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      // const snapshot = await getDocs(collection(firestore, "prompts"));
-
-      // const data = snapshot.docs.map((doc) => ({
-      //   id: doc.id,
-      //   ...doc.data(),
-      // }));
-      const data = await getPromptData()
-
+      const data = await getPromptData();
       setData(data);
     };
 
